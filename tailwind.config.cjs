2963218190ecb827ac696e9693f5b161241eea2f
Niroyan Kugaruban <k.niroyan@gmail.com>
2024-01-24 12:23:06 +0530

    add annosment

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: 'var(--aw-color-primary)',
+        primary: ' #ed9f4c',
         secondary: 'var(--aw-color-secondary)',
         accent: 'var(--aw-color-accent)',
         default: 'var(--aw-color-text-default)',
